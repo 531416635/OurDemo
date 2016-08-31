c2e781dcea5e3290ec0638277fb1d02e4bb88124
yaoyuxiao <531416635@qq.com>
2016-08-31 16:34:40 +0800

    第一次测试文件上传通过

--- OurDemo-core/src/main/java/com/yao/controller/WebController.java
+++ OurDemo-core/src/main/java/com/yao/controller/WebController.java
@@ -1,33 +1,105 @@
 package com.yao.controller;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
+import org.springframework.web.multipart.commons.CommonsMultipartResolver;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
 
 @Controller
-@RequestMapping(value="/web")
+@RequestMapping(value = "/web")
 public class WebController {
-	
-	private static final Logger logger=LoggerFactory.getLogger(WebController.class);
-	
+
+	private static final Logger logger = LoggerFactory
+			.getLogger(WebController.class);
+	int i=0;
 	/**
 	 * 跳转到网站首页
+	 * 
 	 * @return
 	 */
-	@RequestMapping(value="/index")
-	public String index(){
+	@RequestMapping(value = "/index")
+	public String index() {
 		return "website/index";
 	}
-	
+
 	/**
 	 * 班级相册
+	 * 
 	 * @return
 	 */
-	@RequestMapping(value="/toalbum")
-	public String toalbum(){
-		
+	@RequestMapping(value = "/toalbum")
+	public String toalbum() {
+		logger.info("toalbum  toalbum  toalbum");
 		return "website/album";
 	}
-	
+
+	/**
+	 * 班级相册
+	 * 
+	 * @return
+	 * @throws IOException
+	 * @throws IllegalStateException
+	 */
+	@ResponseBody
+	@RequestMapping(value = "/toalbumajax")
+	public JSONObject  toalbumajax(HttpServletRequest request,
+			HttpServletResponse response) throws IllegalStateException,
+			IOException {
+		logger.info("测试图片ajax上传效果");
+		CommonsMultipartResolver multipartResolver = new CommonsMultipartResolver(
+				request.getSession().getServletContext());
+		// 判断 request 是否有文件上传,即多部分请求
+		if (multipartResolver.isMultipart(request)) {
+			// 转换成多部分request
+			MultipartHttpServletRequest multiRequest = (MultipartHttpServletRequest) request;
+			// 取得request中的所有文件名
+			Iterator<String> iter = multiRequest.getFileNames();
+			while (iter.hasNext()) {
+				// 取得上传文件
+				MultipartFile file = multiRequest.getFile(iter.next());
+				if (file != null) {
+					// 取得当前上传文件的文件名称
+					String myFileName = file.getOriginalFilename();
+					// 如果名称不为“”,说明该文件存在，否则说明该文件不存在
+					if (myFileName.trim() != "") {
+						//获取文件后缀名
+						String houzhuiming=myFileName.substring(myFileName.lastIndexOf(".")).toLowerCase();
+						// 定义上传路径
+						String path = request.getSession().getServletContext()
+								.getRealPath(File.separator);
+						File webrootPath = new File(path + "/brandImg/");
+						if (!webrootPath.isDirectory()) {
+							webrootPath.mkdirs();
+						}
+						// 重命名上传后的文件名
+						path = webrootPath  + "/id"+i
+								+ houzhuiming;
+						i++;
+						File localFile = new File(path);
+						file.transferTo(localFile);
+					}
+				}
+			}
+
+		}
+		return JSONObject.parseObject("{'event':'1', 'data':'2', 'previewId':'3', 'index':'4'}");
+	}
 }
--- OurDemo-web/src/main/webapp/pages/website/album.jsp
+++ OurDemo-web/src/main/webapp/pages/website/album.jsp
@@ -1,12 +1,59 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
+<%
+	String path = request.getContextPath();
+	String basePath = request.getScheme() + "://"
+			+ request.getServerName() + ":" + request.getServerPort()
+			+ path + "/";
+%>
+
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 <title>我的相册</title>
+
+<link href="<%=path%>/css/fileinput.css" media="all" rel="stylesheet" type="text/css" />
 </head>
 <body>
-
+	<script src="<%=path%>/js/jquery.min.js"></script>
+	
+	<input id="input-700" name="filename" type="file" multiple="true" class="file-loading">
+	
+	<script src="<%=path%>/js/fileinput.js" type="text/javascript"></script>
+	<script src="<%=path%>/js/zh.js" type="text/javascript"></script>
+	<script>
+		$("#input-700").fileinput({
+			language:"zh",
+			uploadUrl : "http://localhost:8888/OurDemo/web/toalbumajax.do", // server upload action
+			uploadAsync: true,
+		    showUpload: true, // hide upload button
+		    showRemove: true, // hide remove button
+		    minFileCount: 1,
+		    maxFileCount: 10,
+	        //dropZoneEnabled:false,
+	       // resizeImage: false,
+	        //showCaption: false,
+	        //showPreview: false,
+		}).on("filebatchselected", function(event, files) {
+            $(this).fileinput("upload");
+        });
+       /*  .on("fileuploaded", function(event, data,xhr) {
+        	        var obj = JSON.parse(data);
+			        alert('处理成功');
+    }); */
+		
+		
+		$('#input-700').on('fileuploaded', function(event, data, previewId, index) {
+			console.log(event);
+			console.log(data);
+			console.log(previewId);
+			console.log(index);
+			var form = data.form, files = data.files, extra = data.extra,
+		        response = data.response, reader = data.reader;
+		    console.log('File uploaded triggered');
+		}); 
+	</script>
+	
 </body>
 </html>
